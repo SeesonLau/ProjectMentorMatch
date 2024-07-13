b29bc4380850279e4833369574343cb11125811a
Jamel Hadjirasul <hadjirasuljamel80@gmail.com>
2024-07-13 09:56:14 +0800

    class Account added

---
+++ ProjectMentorMatch/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMentorMatch
+{
+    internal class Account
+    {
+        private string userID;
+        private string fullname;
+        private string email;
+        private string password;
+        public Account(string userID, string fullname, string email, string password)
+        {
+            this.userID = userID;
+            this.fullname = fullname;
+            this.email = email;
+            this.password = password;
+        }
+        public string UserID
+        {
+            get { return userID; }
+            set { userID = value; }
+        }
+        public string Fullname
+        {
+            get { return fullname; }
+            set { fullname = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
