664cdda3eaa856452f1e4b436187ab076ee02a0f
Kharylle Iglesias <kharylleann.iglesias8@gmail.com>
2024-07-13 21:42:51 +0800

    Commit para pull

---
+++ ProjectMentorMatch/Views/Dashboard.xaml.cs
@@ -0,0 +1,13 @@
+using Microsoft.Maui.Controls;
+using ProjectMentorMatch.Models;
+using System;
+
+namespace ProjectMentorMatch;
+
+public partial class Dashboard : ContentPage
+{
+	public Dashboard()
+	{
+		InitializeComponent();
+	}
+}
