33e27d0351659daff70b080de36c233857583e6d
John Laurence G. Sison <seesonjohnlau@gmail.com>
2024-07-13 15:12:33 +0800

    Connected Database connectionstring to Account

--- ProjectMentorMatch/Models/Account.cs
+++ ProjectMentorMatch/Models/Account.cs
@@ -5,21 +5,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProjectMentorMatch
+namespace ProjectMentorMatch.Models
 {
-    internal class Account
+    internal class Account : Database
     {
-        private int userID;
+        RandomID randomID = new RandomID();
+
+        private int userID = RandomID.userID();
         private string fullname;
         private string email;
         private string password;
 
-        private static string connectionString = "Server=tcp:mentor-mentee.database.windows.net,1433;Initial Catalog=mentorMentee_DB;Persist Security Info=False;User ID=mentor_mentee;Password=(MSAD12345);MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        //private static string connectionString = "Server=tcp:mentor-mentee.database.windows.net,1433;Initial Catalog=mentorMentee_DB;Persist Security Info=False;User ID=mentor_mentee;Password=(MSAD12345);MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         public Account(string fullname, string email, string password)
         {
-            Random random = new Random();
-            this.userID = random.Next(100000, 999999); // Generates a random 6-digit number
             this.fullname = fullname;
             this.email = email;
             this.password = password;
@@ -48,9 +49,10 @@ namespace ProjectMentorMatch
             get { return password; }
             set { password = value; }
         }
-
+        /*
         public void SignUp()
         {
+
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 string query = "INSERT INTO CreateAccount (userID, fullname, email, password) VALUES (@UserID, @Fullname, @Email, @Password)";
@@ -65,6 +67,23 @@ namespace ProjectMentorMatch
                     connection.Open();
                     command.ExecuteNonQuery();
                 }
+            }
+        }
+        */
+        public void SignUp()
+        {
+
+            string query = "INSERT INTO CreateAccount (userID, fullname, email, password) VALUES (@UserID, @Fullname, @Email, @Password)";
+            using (var connection = GetConnection())
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@UserID", userID);
+                command.Parameters.AddWithValue("@Fullname", fullname);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Password", password);
+
+                connection.Open();
+                command.ExecuteNonQuery();
             }
         }
     }
