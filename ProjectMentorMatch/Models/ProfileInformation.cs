67aca1c3815542adc0b803e2c73f92ab5379a8db
John Laurence G. Sison <seesonjohnlau@gmail.com>
2024-07-15 02:07:01 +0800

    Added fields for Class ProfileInformation part 2

--- ProjectMentorMatch/Models/ProfileInformation.cs
+++ ProjectMentorMatch/Models/ProfileInformation.cs
@@ -26,8 +26,70 @@ namespace ProjectMentorMatch.Models
         private string? addressCity;
         private string? addressProvince;
 
-
-
+        public string? GetSubjectTaught()
+        {
+            return subjectTaught;
+        }
+        public string? GetSubjectTakern()
+        {
+            return subjectTaken;
+        }
+        public string? GetCourseName()
+        {
+            return courseName;
+        }
+        public string? GetDay()
+        {
+            return day;
+        }
+        public double GetInitialTime()
+        {
+            return initialTime;
+        }
+        public double GetFinalTime()
+        {
+            return finalTime;
+        }
+        public string? GetAddressCity()
+        {
+            return addressCity;
+        }
+        public string? GetAddressProvince()
+        {
+            return addressProvince;
+        }
+        public void SetSubjectTaught(string subjectTaught)
+        {
+            this.subjectTaught = subjectTaught;
+        }
+        public void SetSubjectTaken(string subjectTaken)
+        {
+            this.subjectTaken = subjectTaken;
+        }
+        public void SetCourseName(string courseName)
+        {
+            this.courseName = courseName;
+        }
+        public void SetDay(string day)
+        {
+            this.day = day;
+        }
+        public void SetInitialTime(double initialTime)
+        {
+            this.initialTime = initialTime;
+        }
+        public void SetFinalTime(double finalTime)
+        {
+            this.finalTime = finalTime;
+        }
+        public void SetAddressCity(string addressCity)
+        {
+            this.addressCity = addressCity;
+        }
+        public void SetAddressProvince(string addressProvince)
+        {
+            this.addressProvince = addressProvince;
+        }
 
     }
 }
