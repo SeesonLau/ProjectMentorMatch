4c9767a66d443556fe5a6d2bba6f5c46ddae8c19
John Laurence G. Sison <seesonjohnlau@gmail.com>
2024-07-15 01:27:59 +0800

    Added Fields for Class Analytics

--- ProjectMentorMatch/Models/Analytics.cs
+++ ProjectMentorMatch/Models/Analytics.cs
@@ -8,5 +8,26 @@ namespace ProjectMentorMatch.Models
 {
     public class Analytics : ProfileInformation
     {
+        private int analyticsID;
+        private int brainReact;
+        private double points;
+        private int profileID;
+
+        public int GetAnalyticsID()
+        {
+            return analyticsID;
+        }
+        public int GetBrainReact()
+        {
+            return brainReact;
+        }
+        public double GetPoints()
+        {
+            return points;
+        }
+        public int GetProfileID()
+        {
+            return profileID;
+        }
     }
 }
