f3bda693578f31d8ed1b0860b9c776e28b652dd2
John Laurence G. Sison <seesonjohnlau@gmail.com>
2024-07-15 01:39:49 +0800

    Added fields for Class Notification

--- ProjectMentorMatch/Models/Notification.cs
+++ ProjectMentorMatch/Models/Notification.cs
@@ -8,5 +8,18 @@ namespace ProjectMentorMatch.Models
 {
     public class Notification : Account
     {
+        private int notificationID;
+        private string? notificationText; //notification text needs to be automatic. Make resources for the default texts that is frequently used.
+        private int userID;
+
+        public int GetNotificationID()
+        {
+            return notificationID;
+        }
+        public string? GetNotificationText()
+        {
+            return notificationText;
+        }
+
     }
 }
