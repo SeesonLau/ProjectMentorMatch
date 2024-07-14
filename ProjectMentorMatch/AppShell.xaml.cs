9275f77157306a71785c4606907b0d01b1aed9e3
LKFD <lordkentdinampo@gmail.com>
2024-07-14 16:45:04 +0800

    Adding Additional Routing Codes in AppShell.xaml.cs

--- ProjectMentorMatch/AppShell.xaml.cs
+++ ProjectMentorMatch/AppShell.xaml.cs
@@ -13,6 +13,12 @@ namespace ProjectMentorMatch
             Routing.RegisterRoute(nameof(CreateAccount), typeof(CreateAccount));
             Routing.RegisterRoute(nameof(HomeScreen), typeof(HomeScreen));
             Routing.RegisterRoute(nameof(SignIn), typeof(SignIn));
+            Routing.RegisterRoute(nameof(Chat), typeof(Chat));
+            Routing.RegisterRoute(nameof(Dashboard), typeof(Dashboard));
+            Routing.RegisterRoute(nameof(Notification), typeof(Notification));
+            Routing.RegisterRoute(nameof(Profile), typeof(Profile));
+            Routing.RegisterRoute(nameof(Search), typeof(SearchBar));
+            Routing.RegisterRoute(nameof(SignIn), typeof(SignIn));
         }
     }
 }
