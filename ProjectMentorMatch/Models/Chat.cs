989f27afb1fd7ec7332adb7cef2754ca64a8e605
John Laurence G. Sison <seesonjohnlau@gmail.com>
2024-07-15 01:36:15 +0800

    Added fields for Class Chat

--- ProjectMentorMatch/Models/Chat.cs
+++ ProjectMentorMatch/Models/Chat.cs
@@ -8,6 +8,30 @@ namespace ProjectMentorMatch.Models
 {
     public class Chat : Account
     {
+        private int messageID; //serves like a channel
+        private string? messageText;
+        private int userIDSender;
+        private int userIDReceiver;
 
+        public int GetMessageID()
+        {
+            return messageID;
+        }
+        public string? GetMessageText()
+        {
+            return messageText;
+        }
+        public int GetUserIDSender()
+        {
+            return userIDSender;
+        }
+        public int GetUserIDReceiver()
+        {
+            return userIDReceiver;
+        }
+        public void SetMessageText(string messageText)
+        {
+            this.messageText = messageText;
+        }
     }
 }
