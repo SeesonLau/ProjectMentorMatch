e07c4b530db8edd0a2e129e3b349a2b1fd55b02b
John Laurence G. Sison <seesonjohnlau@gmail.com>
2024-07-15 02:15:04 +0800

    Added fields for Class Settings

--- ProjectMentorMatch/Models/Settings.cs
+++ ProjectMentorMatch/Models/Settings.cs
@@ -8,5 +8,37 @@ namespace ProjectMentorMatch.Models
 {
     public class Settings : ProfileInformation
     {
+        private int password;
+        private string? notification; // notification may be uncessary for setting. if so, then use either string or bool.
+        private string? email;
+        private string? aboutMe;
+
+       /* public int GetPassword()
+        {
+            return password;
+        }*/
+        public string? GetNotificaiton()
+        {
+            return notification;
+        }
+        public string? GetChangeEmail()
+        {
+            return email;
+        }
+        public string? GetChangeAboutMe()
+        {
+            return aboutMe;
+        }
+
+        public void SetPassword(int password)
+        {
+            this.password = password;
+        }
+        /*public void SetEmail(string email)
+        {
+            this.email = email;
+        }*/
+        //public void SetAboutMe(string aboutMe)
+
     }
 }
