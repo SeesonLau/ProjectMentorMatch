64859f61baa5afe8033a58100581226e8d01b7a8
angeliedejer <angeliedejer@gmail.com>
2024-07-20 00:59:16 +0800

    Filter search feature

--- ProjectMentorMatch/Views/Search.xaml.cs
+++ ProjectMentorMatch/Views/Search.xaml.cs
@@ -6,6 +6,10 @@ public partial class Search : ContentPage
 	{
 		InitializeComponent();
 	}
+    private void OnClickedFilter(object sender, EventArgs e)
+    {
+        NavigationDrawer.ToggleDrawer();
+    }
     private void OnClickedMentor(object sender, EventArgs e)
     {
         var mentorName = (sender as Element).BindingContext as string;
@@ -15,5 +19,46 @@ public partial class Search : ContentPage
     {
         var mentorName = (sender as Element).BindingContext as string;
         Navigation.PushAsync(new MenteeSearch());
+    }
+    private void OnGenderCheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value)
+        {
+            RadioButton radioButton = sender as RadioButton;
+            if (radioButton != null)
+            {
+                // Logic when a gender radio button is selected
+                // For example, you can store the selected gender value
+                string selectedGender = radioButton.Content.ToString();
+                Console.WriteLine("Selected Gender: " + selectedGender);
+            }
+        }
+    }
+    private void OnSetupModeCheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value)
+        {
+            RadioButton radioButton = sender as RadioButton;
+            if (radioButton != null)
+            {
+                // Logic when a setup mode radio button is selected
+                string selectedSetupMode = radioButton.Content.ToString();
+                Console.WriteLine("Selected Setup Mode: " + selectedSetupMode);
+            }
+        }
+    }
+
+    private void OnInteractionModeCheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+        if (e.Value)
+        {
+            RadioButton radioButton = sender as RadioButton;
+            if (radioButton != null)
+            {
+                // Logic when an interaction mode radio button is selected
+                string selectedInteractionMode = radioButton.Content.ToString();
+                Console.WriteLine("Selected Interaction Mode: " + selectedInteractionMode);
+            }
+        }
     }
 }
