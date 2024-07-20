3dee1fd12e95482f31ee918cca8b3b6734aebc26
Kharylle Iglesias <kharylleann.iglesias8@gmail.com>
2024-07-20 20:37:18 +0800

    Gi solve ang katong wala na push nga MentorListViewModel ug ItemInfo gi add rako ni para test..I delete ra sad niya ni nako

---
+++ ProjectMentorMatch/Models/ItemInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMentorMatch.Models
+{
+    public class ItemInfo
+    {
+        public int ItemId { get; set; }
+        public string? ItemName { get; set; }
+        public string? ImageSource { get; set; }
+    }
+}
---
+++ ProjectMentorMatch/ViewModels/MentorListViewModel.cs
@@ -0,0 +1,54 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using ProjectMentorMatch.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMentorMatch.ViewModels
+{
+    public partial class MentorListViewModel: ObservableObject, INotifyPropertyChanged
+    {
+        public ObservableCollection<ItemInfo> ItemList { get; set; } = new();
+
+        private ItemInfo _currentItem;
+       
+        public ItemInfo CurrentItem
+        {
+            get => _currentItem;
+            //set => SetProperty(ref _currentItem, value);
+            set
+            {
+                if (_currentItem != value)
+                {
+                    _currentItem = value;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(CurrentItemName)); // Notify change of the ItemName
+                }
+            }
+        }
+
+        public string CurrentItemName => CurrentItem?.ItemName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MentorListViewModel() 
+        {
+            ItemList.Add(new ItemInfo() { ItemId = 1, ItemName = "Jamel", ImageSource = "sample_profile.png" });
+            ItemList.Add(new ItemInfo() { ItemId = 2, ItemName = "Ana", ImageSource = "dotnet_bot.png" });
+            ItemList.Add(new ItemInfo() { ItemId = 3, ItemName = "Lapinig", ImageSource = "model.jpg" });
+            ItemList.Add(new ItemInfo() { ItemId = 4, ItemName = "Sison", ImageSource = "model2.jpg" });
+
+            CurrentItem = ItemList.FirstOrDefault();
+        }
+    }
+}
