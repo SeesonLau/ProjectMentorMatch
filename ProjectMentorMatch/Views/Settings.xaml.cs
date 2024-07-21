01e8f0ceb0f8fdecda8bb8054ae52d1523fc6ed5
LKFD <lordkentdinampo@gmail.com>
2024-07-21 13:16:03 +0800

    Added Toggle Notification Methods

--- ProjectMentorMatch/Views/Settings.xaml.cs
+++ ProjectMentorMatch/Views/Settings.xaml.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Maui.Views;
 using ProjectMentorMatch.ViewModels;
+using Plugin.LocalNotification;
 
 namespace ProjectMentorMatch.Views;
 
@@ -8,6 +9,7 @@ public partial class Settings : ContentPage
 	public Settings()
 	{
 		InitializeComponent();
+        notifSwitch.IsToggled = LoadNotificationSetting();
 	}
 
     private async void ImageButton_Clicked(object sender, EventArgs e)
@@ -44,6 +46,51 @@ public partial class Settings : ContentPage
         {
             Application.Current.MainPage = new NavigationPage(new MainPage());
         }
+    }
+
+    private void notifSwitch_Toggled(object sender, ToggledEventArgs e)
+    {
+        bool isEnabled = e.Value;
+
+        // Save the switch state
+        SaveNotificationSetting(isEnabled);
+
+        // Enable or disable notifications based on switch state
+        if (isEnabled)
+        {
+            // Enable notifications
+            EnableNotifications();
+        }
+        else
+        {
+            // Disable notifications
+            DisableNotifications();
+        }
+    }
+
+    private bool LoadNotificationSetting()
+    {
+        // Retrieve the saved notification setting from preferences or settings
+        return Preferences.Get("NotificationsEnabled", true); // Default to true if not set
+    }
+
+    private void SaveNotificationSetting(bool isEnabled)
+    {
+        // Save the notification setting to preferences or settings
+        Preferences.Set("NotificationsEnabled", isEnabled);
+    }
+
+    private void EnableNotifications()
+    {
+        // Logic to enable notifications
+        // For example, you could configure default notification settings
+    }
+
+    private void DisableNotifications()
+    {
+        // Logic to disable notifications
+        // For example, you might cancel all notifications
+      //  NotificationCenter.Current.CancelAll();
     }
 
 
