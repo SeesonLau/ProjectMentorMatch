2623581e515143f0ce5b1d9a738bb64d6d35b45d
Kharylle Iglesias <kharylleann.iglesias8@gmail.com>
2024-07-21 12:58:19 +0800

    Nag add kog SubjectsViewModel para sa mga subjects for binding

---
+++ ProjectMentorMatch/ViewModels/SubjectsViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMentorMatch.ViewModels
+{
+    public class SubjectsViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<string> _availableSub;
+        private ObservableCollection<string> _selectedSub;
+
+        public ObservableCollection<string> AvailableSub
+        {
+            get => _availableSub;
+            set
+            {
+                _availableSub = value;
+                OnPropertyChanged(nameof(AvailableSub));
+            }
+        }
+
+        public ObservableCollection<string> SelectedSub
+        {
+            get => _selectedSub;
+            set
+            {
+                _selectedSub = value;
+                OnPropertyChanged(nameof(SelectedSub));
+            }
+        }
+
+        public SubjectsViewModel()
+        {
+            AvailableSub = new ObservableCollection<string>
+            {
+                "Business", "Calculus", "Chemistry", "Computer Programming",
+                "Economics", "Engineering", "Foreign Language", "Health/Nutrition",
+                "History", "Literacy", "Mathematics", "Medical",
+                "Philosophy", "Physics", "Science"
+            };
+            SelectedSub = new ObservableCollection<string>();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
