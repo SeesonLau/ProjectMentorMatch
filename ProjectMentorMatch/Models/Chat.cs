e19b9375728d04a3a2fae6d8c8e922f68c51cac1
SibuyasGaid <charlesluisgaid@gmail.com>
2024-07-21 15:08:02 +0800

    The Chat Home Page can Load the Data from the SQL database. changed Chat.xaml.cs and chat.xaml

--- ProjectMentorMatch/Models/Chat.cs
+++ ProjectMentorMatch/Models/Chat.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +11,18 @@ namespace ProjectMentorMatch.Models
     {   
         private int messageID = RandomID.messageID(); //serves like a channel
         private string? messageText;
+        private string? senderName;
+        private string receiverName;
         private int userIDSender;
         private int userIDReceiver;
-        private string? timeSent { get; set; } //Will add more fields as needed
+        private DateTime timeSent { get; set; } //Will add more fields as needed
 
+
+        public DateTime GetTimeSent() {  return timeSent; }
+
+        public string? GetSenderName() { return senderName; }
+
+        public string? GetReceiverName() { return receiverName; }   
         public int GetMessageID()
         {
             return messageID;
@@ -34,8 +43,23 @@ namespace ProjectMentorMatch.Models
         {
             this.messageText = messageText;
         }
+
+
+        public void LoadMessages(int userID)
+        {
+
+            string query = "SELECT messageText, userID_sender, userID_receiver FROM Chat WHERE messageID = @messageID";
+            using (var connection = GetConnection())
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                // command.Parameters.AddWithValue("@UserID", userID);
+
+
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+        }
+
     }
-
-
-
 }
--- ProjectMentorMatch/Views/Chat.xaml.cs
+++ ProjectMentorMatch/Views/Chat.xaml.cs
@@ -1,4 +1,8 @@
 using Microsoft.Maui.Controls;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ProjectMentorMatch.Models;
 
 namespace ProjectMentorMatch.Views
 {
@@ -7,6 +11,86 @@ namespace ProjectMentorMatch.Views
         public Chat()
         {
             InitializeComponent();
+            LoadMentorsAndChatsAsync();
+        }
+
+        private async Task LoadMentorsAndChatsAsync()
+        {
+            var mentors = await FetchTopMentorsAsync();
+            var chats = await FetchTopChatsAsync();
+
+            BindMentorsToUI(mentors);
+            BindChatsToUI(chats);
+        }
+
+        private async Task<List<string>> FetchTopMentorsAsync()
+        {
+            var mentors = new List<string>();
+            string query = "SELECT TOP 5 ReceiverName FROM Chat2"; // Modify the query as per your database schema
+
+            using (var connection = new SqlConnection("Server=tcp:mentor2.database.windows.net,1433;Initial Catalog=mentor-mentee2;Persist Security Info=False;User ID=mentormentee2;Password=#MSADSaging;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        mentors.Add(reader.GetString(0));
+                    }
+                }
+            }
+
+            return mentors;
+        }
+
+        private async Task<List<(string ReceiverName, string Message)>> FetchTopChatsAsync()
+        {
+            var chats = new List<(string, string)>();
+            string query = "SELECT TOP 5 ReceiverName, message FROM Chat2"; 
+
+            using (var connection = new SqlConnection("Server=tcp:mentor2.database.windows.net,1433;Initial Catalog=mentor-mentee2;Persist Security Info=False;User ID=mentormentee2;Password=#MSADSaging;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"))
+            using (var command = new SqlCommand(query, connection))
+            {
+                connection.Open();
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        chats.Add((reader.GetString(0), reader.GetString(1)));
+                    }
+                }
+            }
+
+            return chats;
+        }
+
+        private void BindMentorsToUI(List<string> mentors)
+        {
+            var mentorLabels = new List<Label> { Mentor1Label, Mentor2Label, Mentor3Label, Mentor4Label, Mentor5Label };
+
+            for (int i = 0; i < mentors.Count; i++)
+            {
+                mentorLabels[i].Text = mentors[i];
+            }
+        }
+
+        private void BindChatsToUI(List<(string ReceiverName, string Message)> chats)
+        {
+            var chatLabels = new List<(Label name, Label message)>
+            {
+                (Chat1ReceiverLabel, Chat1MessageLabel),
+                (Chat2ReceiverLabel, Chat2MessageLabel),
+                (Chat3ReceiverLabel, Chat3MessageLabel),
+                (Chat4ReceiverLabel, Chat4MessageLabel),
+                (Chat5ReceiverLabel, Chat5MessageLabel)
+            };
+
+            for (int i = 0; i < chats.Count; i++)
+            {
+                chatLabels[i].name.Text = chats[i].ReceiverName;
+                chatLabels[i].message.Text = chats[i].Message;
+            }
         }
 
         async void OnMentorClicked(object sender, EventArgs e)
