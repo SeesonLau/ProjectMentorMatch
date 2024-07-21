ade2fc8b0567f833bbae17bc2b0769ae3d61e1d0
Kharylle Iglesias <kharylleann.iglesias8@gmail.com>
2024-07-21 13:12:58 +0800

    Pushing ProfileViewModels

---
+++ ProjectMentorMatch/ViewModels/ProfileViewModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMentorMatch.ViewModels
+{
+    public class ProfileViewModels : INotifyPropertyChanged
+    {
+        public ScheduleViewModel Schedule { get; set; }
+        public SubjectsViewModel Subjects { get; set; }
+
+        public ProfileViewModels()
+        {
+            Schedule = new ScheduleViewModel();
+            Subjects = new SubjectsViewModel();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
