0e852c98a2611443178c75e3a2039f1fa8d5d8d0
Jamel Hadjirasul <hadjirasuljamel80@gmail.com>
2024-07-21 10:45:05 +0800

    Added methods for analytics-  not tested yet

--- ProjectMentorMatch/Models/Chat.cs
+++ ProjectMentorMatch/Models/Chat.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace ProjectMentorMatch.Models
 {
     public class Chat : Account
-    {
+    {   
         private int messageID = RandomID.messageID(); //serves like a channel
         private string? messageText;
         private int userIDSender;
