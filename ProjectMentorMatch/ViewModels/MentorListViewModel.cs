1069a9707cff6f85e321f91b51380bc0fa870077
LKFD <lordkentdinampo@gmail.com>
2024-07-23 00:23:30 +0800

    Pa Bind ko Kharylle or sa kinsa kaybaw maka bind sa dashboard ani, ang fullname rajud mo gana

--- ProjectMentorMatch/ViewModels/MentorListViewModel.cs
+++ ProjectMentorMatch/ViewModels/MentorListViewModel.cs
@@ -27,14 +27,29 @@ namespace ProjectMentorMatch.ViewModels
             {
                 if (_currentItem != value)
                 {
+                    // Binding Declaration
                     _currentItem = value;
                     OnPropertyChanged();
                     OnPropertyChanged(nameof(CurrentItemName)); // Notify change of the ItemName
+                    OnPropertyChanged(nameof(CurrentItemAddressCity)); // Notify change of the address city
+                    OnPropertyChanged(nameof(CurrentItemAddressProvince)); // Notify change of the address province
+                    OnPropertyChanged(nameof(CurrentItemAboutMe)); // Notify change of the about me
+                    OnPropertyChanged(nameof(CurrentItemQualifications)); // Notify change of the qualifications
+
                 }
             }
         }
 
+
+        // To be put in the Binding of the Dashboard
         public string CurrentItemName => CurrentItem?.ItemName;
+        public string CurrentItemAddressCity => CurrentItem?.addressCity;
+        public string CurrentItemAddressProvince => CurrentItem?.addressProvince;
+        public string CurrentItemAboutMe => CurrentItem?.aboutMe;
+        public string CurrentItemQualifications => CurrentItem?.qualifications;
+
+
+
 
         public event PropertyChangedEventHandler PropertyChanged;
 
