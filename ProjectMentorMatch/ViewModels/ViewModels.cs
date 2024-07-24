252659c8d902f731e71bdb908663e09dc75976d7
Kharylle Iglesias <kharylleann.iglesias8@gmail.com>
2024-07-24 19:17:33 +0800

    Push mo yan Te pt.2

---
+++ ProjectMentorMatch/ViewModels/ViewModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectMentorMatch.ViewModels
+{
+    public class ViewModels : INotifyPropertyChanged
+    {
+        public ScheduleViewModel Schedule { get; set; }
+        public SubjectsViewModel Subjects { get; set; }
+        public ProfileViewModel Profile { get; set; }
+        public ViewModels()
+        {
+            Schedule = new ScheduleViewModel();
+            Subjects = new SubjectsViewModel();
+            Profile = new ProfileViewModel();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
